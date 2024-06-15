b6455da3e994ed1f0f8565b38fcce218c954027f
krarpit <kumar.arpit77@yahoo.com>
2024-06-14 19:33:45 -0700

    pull sgcp submodule

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 extern crate prost_build;
 
 fn main() {
-    prost_build::compile_protos(&["src/sgcp/sgcp.proto"],
-                                &["src/"]).unwrap();
+    prost_build::compile_protos(&["./sgcp/sgcp.proto"],
+                                &["./sgcp"]).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,7 @@ async fn main() {
     let listener = TcpListener::bind("127.0.0.1:4760").await.unwrap();
     loop {
         let (stream, _) = listener.accept().await.unwrap();
-        tokio::spawn(async move {
-            handle_connection(stream).await.unwrap();
-        });
+        handle_connection(stream).await.unwrap();
     }
 }
 
