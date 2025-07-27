5ae09c0fcb47c729eb0e539749ca3016580f6815
krarpit <kumar.arpit77@yahoo.com>
2025-07-26 17:31:31 -0700

    implement toggling between command list and responses

--- src/bin/grm/main.rs
+++ src/bin/grm/main.rs
@@ -28,12 +28,18 @@ use widgets::{
     stateful_list::StatefulList,
 };
 
+enum GrmBlock {
+    CommandList,
+    History,
+}
+
 struct App {
     should_quit: bool,
     command_tree: Vec<NestedListNode>,
     command_list: StatefulList<FlattenedListNode>,
     gpm_client: GpmClient,
     responses: StatefulList<GpmResponse>,
+    current_block: GrmBlock,
 }
 
 impl App {
@@ -59,6 +65,7 @@ impl App {
             command_list,
             gpm_client: GpmClient::new(),
             responses: StatefulList::new(),
+            current_block: GrmBlock::CommandList,
         }
     }
 
@@ -68,6 +75,13 @@ impl App {
             gpm::sgcp::Resource::Bms => Color::LightGreen,
             gpm::sgcp::Resource::Emg => Color::DarkGray,
             gpm::sgcp::Resource::Maestro => Color::Blue,
+        }
+    }
+
+    fn toggle_selected_block(&mut self) {
+        match self.current_block {
+            GrmBlock::CommandList => self.current_block = GrmBlock::History,
+            GrmBlock::History => self.current_block = GrmBlock::CommandList,
         }
     }
 
@@ -88,47 +102,59 @@ impl App {
     fn handle_key_events(&mut self, key: KeyEvent) {
         match key.code {
             KeyCode::Char('q') => self.should_quit = true,
-            KeyCode::Down | KeyCode::Char('j') => self.command_list.next(),
-            KeyCode::Up | KeyCode::Char('k') => self.command_list.previous(),
-            KeyCode::Char('p') => self.responses.previous(),
-            KeyCode::Enter => self.command_list.state.selected().map_or((), |index| {
-                let node = self.command_list.items.get_mut(index).unwrap();
+            KeyCode::Down | KeyCode::Char('j') => match self.current_block {
+                GrmBlock::History => self.responses.next(),
+                GrmBlock::CommandList => self.command_list.next(),
+            },
+            KeyCode::Up | KeyCode::Char('k') => match self.current_block {
+                GrmBlock::History => self.responses.previous(),
+                GrmBlock::CommandList => self.command_list.previous(),
+            },
+            KeyCode::Char('t') => self.toggle_selected_block(),
+            KeyCode::Enter => {
+                if let GrmBlock::CommandList = self.current_block {
+                    self.command_list.state.selected().map_or((), |index| {
+                        let node = self.command_list.items.get_mut(index).unwrap();
 
-                if node.is_root() {
-                    node.toggle_expansion(&mut self.command_tree);
+                        if node.is_root() {
+                            node.toggle_expansion(&mut self.command_tree);
 
-                    self.command_list =
-                        StatefulList::with_items(NestedListNode::flatten(&self.command_tree));
+                            self.command_list = StatefulList::with_items(NestedListNode::flatten(
+                                &self.command_tree,
+                            ));
 
-                    self.command_list
-                        .state
-                        .select(Some(FlattenedListNode::clamp_selection(
-                            self.command_list.items.len(),
-                            index,
-                        )))
-                } else {
-                    let root = node.path.get(0).unwrap();
-                    let root_node = self.command_tree.get(*root).unwrap();
-                    let task_code = node.path.get(1).unwrap();
+                            self.command_list.state.select(Some(
+                                FlattenedListNode::clamp_selection(
+                                    self.command_list.items.len(),
+                                    index,
+                                ),
+                            ))
+                        } else {
+                            let root = node.path.get(0).unwrap();
+                            let root_node = self.command_tree.get(*root).unwrap();
+                            let task_code = node.path.get(1).unwrap();
 
-                    let response = self
-                        .gpm_client
-                        .send(
-                            gpm::sgcp::Resource::from_str_name(root_node.name.as_str()).unwrap(),
-                            *task_code as i32 + 1, // +1 since we don't render task 0
-                        )
-                        .unwrap();
-                    self.responses.items.push(response);
-                    self.responses
-                        .state
-                        .select(Some(self.responses.items.len() - 1));
+                            let response = self
+                                .gpm_client
+                                .send(
+                                    gpm::sgcp::Resource::from_str_name(root_node.name.as_str())
+                                        .unwrap(),
+                                    *task_code as i32 + 1, // +1 since we don't render task 0
+                                )
+                                .unwrap();
+                            self.responses.items.push(response);
+                            self.responses
+                                .state
+                                .select(Some(self.responses.items.len() - 1));
 
-                    self.responses.scrollbar_state = self
-                        .responses
-                        .scrollbar_state
-                        .position(self.responses.items.len() - 1);
+                            self.responses.scrollbar_state = self
+                                .responses
+                                .scrollbar_state
+                                .position(self.responses.items.len() - 1);
+                        }
+                    })
                 }
-            }),
+            },
             _ => {},
         }
     }
@@ -141,6 +167,8 @@ impl App {
         let left_column = main_chunks[0];
         let right_column_chunks =
             Layout::vertical([Constraint::Length(3), Constraint::Min(0)]).split(main_chunks[1]);
+
+        // Status Block
 
         let right_panel_top = Block::default()
             .title("Status")
@@ -156,33 +184,38 @@ impl App {
 
         frame.render_widget(right_panel_top, right_column_chunks[0]);
 
-        let right_panel_bottom = Block::default()
+        // History Block
+
+        let mut right_panel_bottom = Block::default()
             .title("History")
             .borders(Borders::ALL)
             .border_type(BorderType::Rounded)
             .padding(Padding::new(1, 1, 0, 0));
+
         let inner_area = right_panel_bottom.inner(right_column_chunks[1]);
+
+        if let GrmBlock::History = self.current_block {
+            right_panel_bottom = right_panel_bottom.border_style(Style::default().bold());
+        }
 
         frame.render_widget(right_panel_bottom, right_column_chunks[1]);
 
         if !self.responses.items.is_empty() {
-            frame.render_stateful_widget(
-                List::new(self.responses.items.iter().map(|v| {
-                    ListItem::new(Text::from(vec![Line::from(vec![
-                        Span::styled(
-                            format!("{}::{}", v.resource.as_str_name(), &v.task_code),
-                            Style::default()
-                                .fg(Self::get_palette(v.resource))
-                                .add_modifier(Modifier::BOLD),
-                        ),
-                        Span::styled(" ", Style::default()),
-                        Span::styled(&v.message, Style::default()),
-                    ])]))
-                }))
-                .highlight_style(Style::default().bold()),
-                inner_area,
-                &mut self.responses.state,
-            );
+            let base_widget = List::new(self.responses.items.iter().map(|v| {
+                ListItem::new(Text::from(vec![Line::from(vec![
+                    Span::styled(
+                        format!("{}::{}", v.resource.as_str_name(), &v.task_code),
+                        Style::default()
+                            .fg(Self::get_palette(v.resource))
+                            .add_modifier(Modifier::BOLD),
+                    ),
+                    Span::styled(" ", Style::default()),
+                    Span::styled(&v.message, Style::default()),
+                ])]))
+            }))
+            .highlight_style(Style::default().bold());
+
+            frame.render_stateful_widget(base_widget, inner_area, &mut self.responses.state);
         }
 
         let scrollbar = Scrollbar::new(ScrollbarOrientation::VerticalRight)
@@ -201,23 +234,28 @@ impl App {
             &mut self.responses.scrollbar_state,
         );
 
-        frame.render_stateful_widget(
-            List::new(self.command_list.items.iter().map(|v| ListItem::new(v)))
-                .block(
-                    Block::default()
-                        .title("Resources")
-                        .borders(Borders::all())
-                        .border_type(BorderType::Rounded)
-                        .padding(Padding::new(1, 1, 0, 0)),
-                )
-                .highlight_style(
-                    Style::default()
-                        .bg(Color::DarkGray)
-                        .add_modifier(Modifier::BOLD),
-                ),
-            left_column,
-            &mut self.command_list.state,
-        )
+        // Command List Block
+
+        let base_widget = List::new(self.command_list.items.iter().map(|v| ListItem::new(v)))
+            .block(
+                Block::default()
+                    .title("Resources")
+                    .borders(Borders::all())
+                    .border_type(BorderType::Rounded)
+                    .padding(Padding::new(1, 1, 0, 0))
+                    .border_style(if let GrmBlock::CommandList = self.current_block {
+                        Style::default().bold()
+                    } else {
+                        Style::default()
+                    }),
+            )
+            .highlight_style(
+                Style::default()
+                    .bg(Color::DarkGray)
+                    .add_modifier(Modifier::BOLD),
+            );
+
+        frame.render_stateful_widget(base_widget, left_column, &mut self.command_list.state)
     }
 }
 
